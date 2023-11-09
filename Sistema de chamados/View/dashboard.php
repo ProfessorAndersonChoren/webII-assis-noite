d34dfabb158633222b67ddb75d22c59fc6a056ec
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2023-11-09 19:48:35 -0300

    Sistema de chamados - Autenticação

--- Sistema de chamados/View/dashboard.php
+++ Sistema de chamados/View/dashboard.php
@@ -1,6 +1,6 @@
+<?php require_once dirname(__DIR__) . "/Controller/Auth_Verify.php"; ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
