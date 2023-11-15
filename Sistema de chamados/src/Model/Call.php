135c0a9956917fd1a5679c9220f765d091658f0d
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2023-11-14 21:55:32 -0300

    Sistema de chamados - Método construtor

--- Sistema de chamados/src/Model/Call.php
+++ Sistema de chamados/src/Model/Call.php
@@ -2,5 +2,17 @@
 
 namespace QI\SistemaDeChamados\Model;
 class Call{
+    private $id;
+    private $user;
+    private $equipment;
+    private $classification;
+    private $description;
+    private $notes;
 
+    public function __construct($user,$equipment,$classification,$description){
+        $this->user = $user;
+        $this->equipment = $equipment;
+        $this->classification = $classification;
+        $this->description = $description;
+    }
 }
